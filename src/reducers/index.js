48b7d5100a634dcfa74dab1cfe6ef4bdf0d1c87e
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-12-15 14:39:00 +0100

    Finished store for now

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import auth from "./auth";
 import user from "./user";
+import profile from "./profile";
 
 export default combineReducers({
   auth,
   user,
+  profile,
 });
