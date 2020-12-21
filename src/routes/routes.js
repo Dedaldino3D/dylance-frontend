ec915fe678c3188c71d0dcd6123384565dfae1ec
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-12-21 16:24:32 +0100

    Add new routes:     - Profile     - ProfileInfo     - JobsHistory     - Messages     - Notifications

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,36 @@
 import Feed from "../components/Feed";
 import UserInfoFeed from "../components/UserInfoFeed";
 import TopFrelancers from "../components/TopFrelancers";
+import Profile from "../components/Profile";
+import ProfileInfo from "../components/ProfileInfo";
+import JobsHistory from "../components/JobsHistory";
+import Messages from "../components/Messages";
+import Notifications from "../components/Notifications";
 
 const Routes = [
   {
     path: "/home",
-    exact: true,
     home: () => <UserInfoFeed />,
     main: () => <Feed />,
     sidebar: () => <TopFrelancers />,
+  },
+  {
+    path: "/profile",
+    home: () => <ProfileInfo />,
+    main: () => <Profile />,
+    sidebar: () => <JobsHistory />,
+  },
+  {
+    path: "/messages",
+    home: null,
+    main: () => <Messages />,
+    sidebar: null,
+  },
+  {
+    path: "/notifications",
+    home: null,
+    main: () => <Notifications />,
+    sidebar: null,
   },
 ];
 
