062e900bbf7ddf1daf85c3dc6a51112155be5ae8
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-08 12:13:29 +0100

    Add composewithdevtools middleware in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 import loadingBarMiddleware from "../middlewares/loadingBarMiddleware";
 import rootReducer from "../reducers";
 
 const middlewares = [thunk, loadingBarMiddleware];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
