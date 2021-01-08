70838754dc9744fa24e78109214844dc4ce4ca9c
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-08 12:14:20 +0100

    Add display job route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,15 @@ import MainContainer from "../components/App/MainContainer";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
 import NotFound from "../components/NotFound";
+import DisplayJob from "../components/DisplayJob";
+
 
 const DefaultRoutes = () => (
   <Router>
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
+      <Route path="/jobs/:jobSlug" component={DisplayJob} />
       <Route component={MainContainer} />
       <Route component={NotFound} />
     </Switch>
